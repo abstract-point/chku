00582549a95c3d006d01d464385265c972d45864
Ivan Lysenko <diparon@yandex.ru>
2026-04-26 13:54:07 +0300

    Install Vue Query API client dependencies

--- apps/chku-frontend/src/main.ts
+++ apps/chku-frontend/src/main.ts
@@ -2,13 +2,23 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: true,
+    },
+  },
+})
 
 app.use(createPinia())
+app.use(VueQueryPlugin, { queryClient })
 app.use(router)
 
 app.mount('#app')
