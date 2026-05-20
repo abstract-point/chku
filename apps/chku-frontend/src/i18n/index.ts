f1837a1bbaa8e65faeaa8d96638fcfa43c1a2c8b
Ivan Lysenko <diparon@yandex.ru>
2026-05-21 02:02:44 +0300

    Install vue-i18n and create Russian dictionary

---
+++ apps/chku-frontend/src/i18n/index.ts
@@ -0,0 +1,11 @@
+import { createI18n } from 'vue-i18n'
+import ru from './ru'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'ru',
+  fallbackLocale: 'ru',
+  messages: { ru },
+})
+
+export default i18n
